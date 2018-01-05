5a25093bdb6e3ac6a5e5c24a3584be838f472089
eliswanson <32949617+eliswanson@users.noreply.github.com>
2018-01-05 07:37:12 -0800

    GraphTests

--- CSharpMaze/CSharpMaze/Graph.cs
+++ CSharpMaze/CSharpMaze/Graph.cs
@@ -7,7 +7,8 @@ namespace CSharpMaze
     [Serializable]
     public class Graph<T>
     {
-        private Dictionary<T, Vertex> Maze { get; } = new Dictionary<T, Vertex>();
+        #region Fields/Constructors
+        private Dictionary<T, Vertex> mazeDictionary { get; } = new Dictionary<T, Vertex>();
 
         /// <summary>
         /// Creates an undirected and unweighted graph that can contain vertices of an user specified type. Does not allow duplicate vertices.
@@ -15,17 +16,21 @@ namespace CSharpMaze
         public Graph()
         {
         }
-
+        #endregion
         #region Vertex Connections
         /// <summary>
-        /// Add a new vertex to the graph.
+        /// Add a new vertex to the graph. Duplicates not allowed, thows an exception.
         /// </summary>
         /// <param name="v"></param>
         public void AddVertex(T v)
         {
             if(v == null)
                 throw new ArgumentNullException();
-            Maze.Add(v, new Vertex()); // needs null check?
+
+            if (mazeDictionary.ContainsKey(v))
+                throw new ArgumentException("Duplicate vertices not allowed; vertices must be unique");
+
+            mazeDictionary.Add(v, new Vertex());
         }
 
         /// <summary>
@@ -35,25 +40,29 @@ namespace CSharpMaze
         /// <returns></returns>
         public bool Contains(T v)
         {
-            return Maze.ContainsKey(v);
+            return mazeDictionary.ContainsKey(v);
         }
         /// <summary>
-        /// Connects vertices. Does not add duplicates. Throws exception if either vertix isn't in the graph.
+        /// Connects vertices. Returns true if succesful, false if failed. Does not add duplicate connections. Throws exception if either vertix isn't in the graph.
         /// </summary>
         /// <param name="v1"></param>
         /// <param name="v2"></param>
         public void Connect(T v1, T v2) //return false if failed?
         {
-            if (!Maze.TryGetValue(v1, out Vertex first))
-                throw new NullReferenceException("v1 does not exist in the graph");
+            if (!mazeDictionary.TryGetValue(v1, out Vertex firstVertex))
+                throw new ArgumentOutOfRangeException("v1 does not exist in the graph");
 
-            if (!Maze.TryGetValue(v2, out Vertex second))
-                throw new NullReferenceException("v2 does not exist in the graph");
+            if (!mazeDictionary.TryGetValue(v2, out Vertex secondVertex))
+                throw new ArgumentOutOfRangeException("v2 does not exist in the graph");
 
-            if(!first.neighbors.Contains(second))
-                first.neighbors.AddLast(second);
-            if(!second.neighbors.Contains(first))
-                second.neighbors.AddLast(first);
+            if (!firstVertex.neighborsList.Contains(secondVertex))
+                firstVertex.neighborsList.AddLast(secondVertex);
+
+            if (!secondVertex.neighborsList.Contains(firstVertex))
+                secondVertex.neighborsList.AddLast(firstVertex);
+
+            if (!IsConnected(v1, v2))
+                throw new Exception("v1 was not succesfully connected to v2; this should never happen");
         }
         /// <summary>
         /// Returns true if vertices are connected. Throws exception if either vertex isn't in the graph.
@@ -63,13 +72,13 @@ namespace CSharpMaze
         /// <returns></returns>
         public bool IsConnected(T v1, T v2)
         {
-            if (!Maze.TryGetValue(v1, out Vertex first))
-                throw new NullReferenceException("v1 does not exist in the graph");      
+            if (!mazeDictionary.TryGetValue(v1, out Vertex firstVertex))
+                throw new ArgumentOutOfRangeException("v1 does not exist in the graph");      
             
-            if (!Maze.TryGetValue(v2, out Vertex second))
-                throw new NullReferenceException("v2 does not exist in the graph");
+            if (!mazeDictionary.TryGetValue(v2, out Vertex secondVertex))
+                throw new ArgumentOutOfRangeException("v2 does not exist in the graph");
 
-            return first.neighbors.Contains(second);
+            return firstVertex.neighborsList.Contains(secondVertex) && secondVertex.neighborsList.Contains(firstVertex);
         }
         /// <summary>
         /// Disconnects vertices. Returns false if they weren't connected. Throws exception either vertex isn't in the graph.
@@ -79,13 +88,13 @@ namespace CSharpMaze
         /// <returns></returns>
         public bool Disconnect(T v1, T v2)
         {
-            if (!Maze.TryGetValue(v1, out Vertex first))
+            if (!mazeDictionary.TryGetValue(v1, out Vertex firstVertex))
                 throw new NullReferenceException("v1 does not exist in the graph");
 
-            if (!Maze.TryGetValue(v2, out Vertex second))
+            if (!mazeDictionary.TryGetValue(v2, out Vertex secondVertex))
                 throw new NullReferenceException("v2 does not exist in the graph");
 
-            return (first.neighbors.Remove(second) && second.neighbors.Remove(first));
+            return (firstVertex.neighborsList.Remove(secondVertex) && secondVertex.neighborsList.Remove(firstVertex));
         }
         #endregion
         #region BFS Operations
@@ -97,30 +106,30 @@ namespace CSharpMaze
         /// <returns></returns>
         public bool BFS(T start, T finish)
         {
-            if (!Maze.TryGetValue(start, out Vertex first))
+            if (!mazeDictionary.TryGetValue(start, out Vertex firstVertex))
                 throw new NullReferenceException("start is not in the graph");
 
-            if (!Maze.TryGetValue(finish, out Vertex find))
+            if (!mazeDictionary.TryGetValue(finish, out Vertex findVertex))
                 throw new NullReferenceException("finish is not in the graph");            
 
             bool found = false;
             Queue<Vertex> queue = new Queue<Vertex>();
-            queue.Enqueue(first);
+            queue.Enqueue(firstVertex);
 
             while (!found && queue.Count > 0)
             {
                 Vertex cur = queue.Dequeue();
 
-                foreach (Vertex n in cur.neighbors)
+                foreach (Vertex v in cur.neighborsList)
                 {
-                    if (n.Equals(find))
+                    if (v.Equals(findVertex))
                     {
                         found = true;
                         break;
                     }
-                    if (n.Visited) continue;
-                    queue.Enqueue(n);
-                    n.Visited = true;
+                    if (v.Visited) continue;
+                    queue.Enqueue(v);
+                    v.Visited = true;
                 }
             }
 
@@ -130,7 +139,7 @@ namespace CSharpMaze
         }
         private void ResetVisited()
         {
-            foreach(Vertex v in Maze.Values)
+            foreach(Vertex v in mazeDictionary.Values)
             {
                 v.Visited = false;
             }
@@ -139,7 +148,7 @@ namespace CSharpMaze
         [Serializable]
         private class Vertex
         {
-            public readonly LinkedList<Vertex> neighbors = new LinkedList<Vertex>();
+            public readonly LinkedList<Vertex> neighborsList = new LinkedList<Vertex>();
             public bool Visited { get; set; }
         }
     }       
--- CSharpMaze/CSharpMazeTests/GraphTests.cs
+++ CSharpMaze/CSharpMazeTests/GraphTests.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using CSharpMaze;
+using SUT = CSharpMaze;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,28 +11,181 @@ namespace CSharpMaze.Tests
     [TestClass()]
     public class GraphTests
     {
+        // MethodName_StateUnderTest_ExpectedBehavior
+        // Can use nested class called MethodName to reduce repitition
         [TestMethod()]
-        public void ContainsFalseTest()
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddVertex_NullVertex_ThrowArgumentNullException()
         {
-            Graph<int> testGraph = new Graph<int>();            
-            Assert.IsFalse(testGraph.Contains(-1));
+            //arrange
+            Graph<object> graph = new SUT.Graph<object>();
+
+            //act and assert
+            graph.AddVertex(null);
         }
         [TestMethod()]
-        public void ContainsTrueTest()
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddVertex_DuplicateAdded_ArgumentException()
         {
-            Graph<int> testGraph = new Graph<int>();
-            testGraph.AddVertex(-1);
-            Assert.IsTrue(testGraph.Contains(-1));
+            //arrange
+            var graph = new SUT.Graph<int>();
+            graph.AddVertex(0);
+
+            //act and assert
+            graph.AddVertex(0);
+        }
+        [TestMethod()]
+        public void Contains_EmptyGraph_False()
+        {
+            //arrange
+            var graph = new SUT.Graph<int>();   
+            
+            //act
+            var result = graph.Contains(0);
+
+            //assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod()]
+        public void Contains_InputNotInGraph_False()
+        {
+            //arrange
+            var graph = new SUT.Graph<int>();
+            graph.AddVertex(1);
+
+            //act
+            var result = graph.Contains(0);
+
+            //and assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod()]
+        public void Contains_InputIsInGraph_True()
+        {
+            //arrange
+            var graph = new SUT.Graph<int>();
+            graph.AddVertex(0);
+
+            //act
+            var result = graph.Contains(0);
+
+            //and assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void IsConnected_Vertex1AndVertex2NotInGraph_ArgumentOutOfRangeException()
+        {
+            //arrange
+            var graph = new SUT.Graph<int>();
+
+            //act and assert
+            var result = graph.IsConnected(0, 2);
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void IsConnected_Vertex1NotInGraphButVertex2Is_ArgumentOutOfRangeException()
+        {
+            //arrange
+            var graph = new SUT.Graph<int>();
+            graph.AddVertex(2);
+
+            //act and assert
+            var result = graph.IsConnected(0, 2);
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void IsConnected_Vertex1IsInGraphButVertext2IsNot_ArgumentOutOfRangeException()
+        {
+            //arrange
+            var graph = new SUT.Graph<int>();
+            graph.AddVertex(0);
+
+            //act and assert
+            var result = graph.IsConnected(0, 2);
+        }
+        [TestMethod()]
+        public void IsConnected_VerticesInGraphButNeverConnected_False()
+        {
+            //arrange
+            var graph = new SUT.Graph<int>();
+            graph.AddVertex(0);
+            graph.AddVertex(2);
+
+            //act
+            var result = graph.IsConnected(0, 2);
+
+            //assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod()]
+        public void IsConnected_VerticesConnected_True()
+        {
+            //arrange
+            var graph = new SUT.Graph<int>();
+            graph.AddVertex(0);
+            graph.AddVertex(2);
+
+            //act
+            graph.Connect(0, 2);
+            var resultOrder1 = graph.IsConnected(0, 2);
+            var resultOrder2 = graph.IsConnected(2, 0);
+
+            //assert
+            Assert.IsTrue(resultOrder1);
+            Assert.IsTrue(resultOrder2);
         }
 
         [TestMethod()]
-        public void AddVertexNullTest()
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Connect_Vertex1NotInGraph_ArgumentOutOfRangeException()
         {
-            Graph<Object> testGraph = new Graph<Object>();            
-            Assert.ThrowsException<ArgumentNullException>(() =>
-            {
-                testGraph.AddVertex(null);
-            });
+            //arrange
+            var graph = new SUT.Graph<int>();
+            graph.AddVertex(2);
+
+            //act and assert
+            graph.Connect(1, 2);
         }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Connect_Vertex2NotInGraph_ArgumentOutOfRangeException()
+        {
+            //arrange
+            var graph = new SUT.Graph<int>();
+            graph.AddVertex(1);
+
+            //act and assert
+            graph.Connect(1, 2);
+        }
+        [TestMethod()]
+        public void Connect_Vertex1AndVertex2Connected_True()
+        {
+            //arrange
+            var graph = new SUT.Graph<int>();
+            graph.AddVertex(1);
+            graph.AddVertex(2);
+
+            //act
+            graph.Connect(1, 2);
+            var result = graph.IsConnected(1, 2);
+
+            //assert
+            Assert.IsTrue(result);
+        }
+
+        /*
+        [TestMethod()]
+        public void Method_StateUnderTest_ExpectedBehavior()
+        {
+            //arrange
+
+            //act
+
+            //assert
+            Assert.IsTrue(false);
+        }
+        */
     }
 }
